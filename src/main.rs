0b7856ce39a3170138ad423310a50a7770c214e1
Andi Bensisva <bensisva@gmail.com>
2017-12-27 13:16:48 +0700

    Memastikan exiting looping

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 extern crate rand;
 
-//use std::io;
+use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("==== Guess The Number ====");
-	let secret_number = rand::thread_rng().gen_range(1,4);
-    println!("{}",secret_number);
-}
+    println!("=== KUIS TEBAK ANGKA ===");	
+    let angka_rahasia = rand::thread_rng().gen_range(1,4);
+
+    loop {
+        println!("Masukan tebakan anda:");
+        let mut tebakan = String::new();
+        io::stdin().read_line(&mut tebakan)
+            .expect("Ulangi tebakan anda!");
+        
+        let tebakan: u32 = match tebakan.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            
+        match tebakan.cmp(&angka_rahasia){
+            Ordering::Less => println!("Masih kekecilan"),
+            Ordering::Greater => println!("Masih kebesaran"),
+            Ordering::Equal => { 
+                println!("Tepat sekali!");
+                break
+            }
+        }
+
+    println!("Angkanya {}, tebakan anda {}",angka_rahasia, tebakan );
+
+    }
+   }
